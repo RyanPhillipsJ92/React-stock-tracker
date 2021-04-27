ae20204d50c40ea262eab7520d525d8b9f89d386
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-04-27 14:14:13 -0500

    first commit

---
+++ src/components/StockRow.js
@@ -0,0 +1,35 @@
+import React, {Component, useCallback} from 'react';
+import { stock } from '../resources/stock.js';
+
+class StockRow extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        }
+    }
+
+    applyData(data) {
+        this.setState({
+            data: data
+        })
+    }
+
+    componentDidMount() {
+        stock.latestPrice(this.props.ticker, this.applyData.bind(this))
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.ticker}</td>
+                <td>{this.state.data.price}</td>
+                <td>{this.state.data.date}</td>
+                <td>{this.state.data.time}</td>
+            </tr>
+        );
+    }
+}
+
+export default StockRow;
---
+++ src/config/iex.js
@@ -0,0 +1,4 @@
+export const iex = {
+    api_token: 'pk_31d9b8579004467098c66f12d1d1aeeb',
+    base_url: 'https://cloud.iexapis.com/stable/'
+};
